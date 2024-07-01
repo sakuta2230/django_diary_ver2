d7df24734fac8f43b9729a9af1b9d7af90f68231
saku <nyansaku2238@yahoo.co.jp>
2024-07-01 12:35:50 +0000

    add login

--- sampleApp/views/login_view2.py
+++ sampleApp/views/login_view2.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login
+#変更
 def loginfunc(request):    
     if request.method=='POST':
         username=request.POST['username']
