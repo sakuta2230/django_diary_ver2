17ccf92773b1e6dc3dafc8659b5ab7d360fae01f
sakuta2230 <nyansaku2238@yahoo.co.jp>
2024-09-05 13:02:40 +0000

    admin.py

--- sampleApp/admin.py
+++ sampleApp/admin.py
@@ -1,6 +1,8 @@
 # sampleApp/admin.py
 from django.contrib import admin
-from .models import article1, health_score  # モデルのインポート
+from sampleApp.model.article1 import article
+from sampleApp.model.health_score import HealthScore
+ # モデルのインポート
 
-admin.site.register(article1)  # article1モデルを管理画面に登録
-admin.site.register(health_score)  # health_scoreモデルを管理画面に登録
+admin.site.register(article)  # article1モデルを管理画面に登録
+admin.site.register(HealthScore)  # health_scoreモデルを管理画面に登録
