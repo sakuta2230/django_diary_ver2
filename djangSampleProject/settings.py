68e78c6cb5e8f728c83a41df0bab49a4d972295a
sakuta2230 <nyansaku2238@yahoo.co.jp>
2024-10-12 04:14:52 +0000

    docker-compose.yml

--- djangSampleProject/settings.py
+++ djangSampleProject/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t=er^lq3b4uzx2gi-l)-phu80s!tz5($9z_3evg6vh0knf97as'
+
+SECRET_KEY = config('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
